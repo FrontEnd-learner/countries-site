97f7f9a963a5c0aec5ac2889fe4348910657afb4
FrontEnd-learner <learnerbeka@gmail.com>
2024-02-17 08:21:11 +0500

    Change form code

--- instruction.md
+++ instruction.md
@@ -1,5 +1,6 @@
 # 1. SHRIFTLAR
 #   Nunito Sans 800, 600, 400, 300
+#   GET qidirish..., POST saqlash
 # 2. XATOLIKLAr
 #   - JS da agar body dark bo'lsa header va itemlar oq bo'lib qolyapti
 #   1. yechim root ni to'g'ri berishda shunda adashibman
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 //   DARK MODE qo'shish
-const elDarkModeButton = document.querySelector('.site-header-theme-toggler')
+const elSiteThemeToggler = document.querySelector('.site-header-theme-toggler')
 
-elDarkModeButton.addEventListener('click', function() {
-    document.body.classList.add('dark-mode');
+elSiteThemeToggler.addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
 });
+// toggle bosganda qo'shilsin yana bosganda olinsin
