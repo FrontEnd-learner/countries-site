a571cefeec092d2dde410beb57f8d369067df05a
FrontEnd-learner <learnerbeka@gmail.com>
2024-02-17 07:40:53 +0500

    Remove js code

--- js/main.js
+++ js/main.js
@@ -3,5 +3,4 @@ const elDarkModeButton = document.querySelector('.site-header-theme-toggler')
 
 elDarkModeButton.addEventListener('click', function() {
     document.body.classList.add('dark-mode');
-    document.header.classList.add('dark-mode');
 });
